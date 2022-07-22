c0b40ccc33b8f8ff96761d699acaa45043b9c918
Ivan Castro <cluend91@gmail.com>
2022-07-22 15:41:03 -0500

    tarea1

---
+++ index.js
@@ -0,0 +1,9 @@
+// Este es eel mensaje de Bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*hola como estas mi nombre
+es ivan Ramiro castro estudio
+programacion */
+
+//Este va a ser el mensaje que se envia para despedir
+console.log("Adios")
